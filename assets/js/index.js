d8db433f6f9ea28e962791181aa9640ef6f72029
Matthew Howell <llmatt556@hotmail.com>
2018-09-06 19:57:32 -0400

    Remove console output

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
     console.log('ready')
 
     $.get('https://node-js-analytics.azurewebsites.net').then(function(res){
-        console.log(res);
+        //console.log(res);
     });
 });
