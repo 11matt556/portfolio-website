a5584dcc99a3e364b11058467e6e568d75c4ccfe
Matthew Howell <llmatt556@hotmail.com>
2018-09-06 19:50:21 -0400

    Remove url slash

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     console.log('ready')
 
-    $.get('https://node-js-analytics.azurewebsites.net/').then(function(res){
+    $.get('https://node-js-analytics.azurewebsites.net').then(function(res){
         console.log(res);
     });
 });
