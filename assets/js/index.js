a5830a727b4f45a4c7cb4d494b18680522a8a9ed
Matthew Howell <mrhowel@g.clemson.edu>
2022-12-07 20:04:36 -0500

    Clear out website for now. It was way out of date and needs to be redone.

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    console.log('ready')
-
-    $.get('https://node-js-analytics.azurewebsites.net/record').then(function(res){
-        //console.log(res);
-    });
-});
